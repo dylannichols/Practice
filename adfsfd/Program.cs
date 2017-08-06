321e7f1da759c3f709e9c0abd2694c9a25a6dd43
Dylan Nichols <nichodo@g.toiohomai.ac.nz>
2017-08-07 10:41:26 +1200

    initial commit

---
+++ adfsfd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adfsfd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
